23a0fc21dafb3548712421c534c131928dbb0aa7
Antony Faris <acp325@gmail.com>
2023-07-19 20:54:27 -0500

    Refactor path handling to use POSIX format (#10)

--- src/workspace/codebase/make-directory/index.ts
+++ src/workspace/codebase/make-directory/index.ts
@@ -1,18 +1,18 @@
-import { basename, dirname, join } from 'path';
+import { basename, dirname, posix } from 'path';
 
 export default (codebase: CodebaseType, file: FileContainerType) => {
   const filename = basename(file.pathname);
 
   const filenamePieces = filename.split('.');
-  const subfolder = filenamePieces[0];
+  const fileBaseName = filenamePieces[0];
 
-  if (subfolder === 'index') {
+  if (fileBaseName === 'index') {
     return file.pathname;
   }
 
   const directory = dirname(file.pathname);
   const newExtension = filenamePieces.slice(1).join('.');
-  const newSrc = join(directory, subfolder, `index.${newExtension}`);
+  const newSrc = posix.join(directory, fileBaseName, `index.${newExtension}`);
   codebase.files[newSrc] = codebase.files[file.pathname];
   delete codebase.files[file.pathname];
   file.pathname = newSrc;
